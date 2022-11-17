b6690d532099b960a0e648524d3521abb94171e5
Octopols <sony.vaio.1404@gmail.com>
2022-11-18 00:41:01 +0530

    added a wallpaper screen when you click on an item. the layout is not final for activity_final_wallpaper.xml fragment_library-layout.xml + work on wallpaper activity

--- app/src/main/java/com/example/wallionary/FinalWallpaper.kt
+++ app/src/main/java/com/example/wallionary/FinalWallpaper.kt
@@ -1,11 +1,18 @@
 package com.example.wallionary
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import com.bumptech.glide.Glide
 import com.example.wallionary.databinding.ActivityFinalWallpaperBinding
 import com.example.wallionary.databinding.ActivityMainBinding
+import com.google.firebase.inject.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.net.URL
 
 class FinalWallpaper : AppCompatActivity() {
 
@@ -17,17 +24,19 @@ class FinalWallpaper : AppCompatActivity() {
         setContentView(binding.root)
 
         val url = intent.getStringExtra("link")
+        val urlImage = URL(url)
 
         binding.imageView3
         Glide.with(this).load(url).into(binding.imageView3)
 
         binding.setWallBtn.setOnClickListener {
-
+            //
         }
 
         binding.downloadBtn.setOnClickListener {
-
+            //
         }
 
     }
+
 }
