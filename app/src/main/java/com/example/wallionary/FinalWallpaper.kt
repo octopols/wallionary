0939001bfab47c61e2c0c89746ea928a244cf2c5
Octopols <sony.vaio.1404@gmail.com>
2022-11-18 07:56:19 +0530

    working download and wallpaper set

--- app/src/main/java/com/example/wallionary/FinalWallpaper.kt
+++ app/src/main/java/com/example/wallionary/FinalWallpaper.kt
@@ -1,9 +1,15 @@
 package com.example.wallionary
 
+import android.app.WallpaperManager
+import android.content.ContentValues
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.example.wallionary.databinding.ActivityFinalWallpaperBinding
 import com.example.wallionary.databinding.ActivityMainBinding
@@ -12,7 +18,12 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
+import java.io.File
+import java.io.OutputStream
 import java.net.URL
+import java.util.*
+import java.io.IOException as IOException1
+import kotlin.random.Random as KotlinRandomRandom
 
 class FinalWallpaper : AppCompatActivity() {
 
@@ -24,19 +35,75 @@ class FinalWallpaper : AppCompatActivity() {
         setContentView(binding.root)
 
         val url = intent.getStringExtra("link")
+
         val urlImage = URL(url)
+
 
         binding.imageView3
         Glide.with(this).load(url).into(binding.imageView3)
 
         binding.setWallBtn.setOnClickListener {
-            //
+            val result: kotlinx.coroutines.Deferred<Bitmap?> = GlobalScope.async {
+                urlImage.toBitmap()
+            }
+
+            GlobalScope.launch(Dispatchers.Main) {
+                val wallpaperManager = WallpaperManager.getInstance(applicationContext)
+                wallpaperManager.setBitmap(result.await())
+            }
+
         }
 
         binding.downloadBtn.setOnClickListener {
-            //
+            val result: kotlinx.coroutines.Deferred<Bitmap?> = GlobalScope.async {
+                urlImage.toBitmap()
+            }
+
+            GlobalScope.launch(Dispatchers.Main) {
+                saveImage(result.await())
+            }
         }
 
     }
+
+    fun URL.toBitmap(): Bitmap? {
+        return try {
+            BitmapFactory.decodeStream(openStream())
+        } catch (e: IOException1) {
+            null
+        }
+
+    }
+
+    private fun saveImage(image: Bitmap?) {
+
+        val random1 = Random().nextInt(520985)
+        val random2 = Random().nextInt(520985)
+
+        val name = "wallionary-${random1 + random2}"
+
+        val data: OutputStream
+        try {
+            val resolver = contentResolver
+            val contentValues = ContentValues()
+            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, "$name.jpg")
+            contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+            contentValues.put(
+                MediaStore.MediaColumns.RELATIVE_PATH,
+                Environment.DIRECTORY_PICTURES + File.separator + "wallionary"
+            )
+            val imageUri =
+                resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+            data = resolver.openOutputStream(Objects.requireNonNull(imageUri)!!)!!
+            image?.compress(Bitmap.CompressFormat.JPEG, 100, data)
+            Objects.requireNonNull<OutputStream?>(data)
+            Toast.makeText(this, "Image Save", Toast.LENGTH_SHORT).show()
+
+        } catch (e: Exception) {
+            Toast.makeText(this, "Image Not Save", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
 
 }
